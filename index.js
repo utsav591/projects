3edc7c6572f3e9f7f22cd01668a21eb6d1912ffd
utsav591 <utsavredekar28@gmail.com>
2023-05-15 22:25:53 +0530

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var dark=document.getElementById("features","baner1","four1");
+    var dark1=document.querySelector("body","button","div","img",'li',"h1");
+    dark.classList.toggle("darkM");
+    dark1.classList.toggle("darkM");
+    console.log("Dark Mode")
+}
